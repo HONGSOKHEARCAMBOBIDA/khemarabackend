75918f1deaafc21e76a8a92dd4e5f32ff646759f
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-04-29 13:19:36 +0700

    update0

--- service/attendance_service.go
+++ service/attendance_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"mysql/config"
 	"mysql/model"
 	"mysql/request"
@@ -39,8 +40,9 @@ func (s *attendanceservice) CheckIn(input request.LocationRequest) error {
 	}()
 
 	now := time.Now()
-	dayOfWeek := int(now.Weekday())
-	dayOfWeek = (dayOfWeek+6)%7 + 1
+	currentDate := now.Format("2006-01-02")
+
+	dayOfWeek := (int(now.Weekday())+6)%7 + 1
 
 	var shiftpattern model.ShiftPattern
 	if err := tx.Where("employee_id = ? AND day_of_week_id = ?", input.EmployeeID, dayOfWeek).
@@ -54,151 +56,104 @@ func (s *attendanceservice) CheckIn(input request.LocationRequest) error {
 		return nil
 	}
 
+	branchLat, err := strconv.ParseFloat(input.BranchLatitude, 64)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	branchLng, err := strconv.ParseFloat(input.BranchLongitude, 64)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	userLat, err := strconv.ParseFloat(input.Latitude, 64)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	userLng, err := strconv.ParseFloat(input.Longitude, 64)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	distance := utils.CalculateDistance(branchLat, branchLng, userLat, userLng)
+	inzone := distance <= float64(input.BranchRadius)
+
 	var attendancelog model.AttendanceLog
-	err := tx.Where("employee_id = ? AND check_date =?", input.EmployeeID, now.Format("2006-01-02")).
+	err = tx.Where("employee_id = ? AND check_date = ?", input.EmployeeID, currentDate).
 		Order("id DESC").
 		First(&attendancelog).Error
 
+	var session model.ShiftSession
+	var shiftOrder int
+
 	if err != nil {
-		var session model.ShiftSession
+
 		if err := tx.Where("shift_id = ?", shiftpattern.ShiftID).
 			Order("shift_order ASC").
 			First(&session).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
-		lat, err := strconv.ParseFloat(input.BranchLatitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		log, err := strconv.ParseFloat(input.BranchLongitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		latput, err := strconv.ParseFloat(input.Latitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		logput, err := strconv.ParseFloat(input.Longitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		distance := utils.CalculateDistance(lat, log, latput, logput)
-		inzone := distance <= float64(input.BranchRadius)
-		currentDate := time.Now().Format("2006-01-02")
-		now := time.Now()
-		start_time, _ := time.Parse("15:04:05", session.StartTime)
-		is_late := 0
-		if now.Hour() > start_time.Hour() || (now.Hour() == start_time.Hour() && now.Minute() > start_time.Minute()) {
-			is_late = 1
-		}
-
-		newlog := model.AttendanceLog{
-			EmployeeID:            input.EmployeeID,
-			CheckDate:             currentDate,
-			Note:                  "",
-			BranchID:              input.BranchID,
-			StatusAttendanceLogID: 1,
-			ShiftSessionOrder:     session.ShiftOrder,
-		}
-
-		if err := tx.Create(&newlog).Error; err != nil {
-			tx.Rollback()
-			return err
-		}
-
-		newlogrecore := model.AttendanceRecord{
-			AttendanceLogID: newlog.ID,
-			ShiftSessionID:  session.ID,
-			CheckTime:       now,
-			IsLate:          &is_late,
-			IsLeftEarly:     nil,
-			Latitude:        lat,
-			Logitude:        log,
-			Note:            input.Note,
-			Iszoone:         inzone,
-			Type:            "IN",
-		}
-
-		if err := tx.Create(&newlogrecore).Error; err != nil {
-			tx.Rollback()
-			return err
-		}
-
+		shiftOrder = session.ShiftOrder
 	} else {
+		if attendancelog.StatusAttendanceLogID == 1 {
+			tx.Rollback()
+			return fmt.Errorf("you don't check-out yet")
+		}
+		shiftOrder = attendancelog.ShiftSessionOrder + 1
 
-		nextOrder := attendancelog.ShiftSessionOrder + 1
-
-		var session model.ShiftSession
-		if err := tx.Where("shift_id = ? AND shift_order = ?", shiftpattern.ShiftID, nextOrder).
+		if err := tx.Where("shift_id = ? AND shift_order = ?", shiftpattern.ShiftID, shiftOrder).
 			First(&session).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
+	}
 
-		lat, err := strconv.ParseFloat(input.BranchLatitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		log, err := strconv.ParseFloat(input.BranchLongitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		latput, err := strconv.ParseFloat(input.Latitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		logput, err := strconv.ParseFloat(input.Longitude, 64)
-		if err != nil {
-			tx.Rollback()
-			return err
-		}
-		distance := utils.CalculateDistance(lat, log, latput, logput)
-		inzone := distance <= float64(input.BranchRadius)
-		now := time.Now()
-		start_time, _ := time.Parse("15:04:05", session.StartTime)
-		currentDate := time.Now().Format("2006-01-02")
-		is_late := 0
-		if now.Hour() > start_time.Hour() || (now.Hour() == start_time.Hour() && now.Minute() > start_time.Minute()) {
-			is_late = 1
-		}
-		newattendancelog := model.AttendanceLog{
-			EmployeeID:            input.EmployeeID,
-			CheckDate:             currentDate,
-			Note:                  "",
-			BranchID:              input.BranchID,
-			StatusAttendanceLogID: 1,
-			ShiftSessionOrder:     nextOrder,
-		}
-		if err := tx.Create(&newattendancelog).Error; err != nil {
-			tx.Rollback()
-			return err
-		}
-		newlogrecore := model.AttendanceRecord{
-			AttendanceLogID: newattendancelog.ID,
-			ShiftSessionID:  session.ID,
-			CheckTime:       now,
-			IsLate:          &is_late,
-			IsLeftEarly:     nil,
-			Latitude:        lat,
-			Logitude:        log,
-			Note:            input.Note,
-			Iszoone:         inzone,
-			Type:            "IN",
-		}
+	startTime, _ := time.Parse("15:04:05", session.StartTime)
+	isLate := 0
+	if now.Hour() > startTime.Hour() ||
+		(now.Hour() == startTime.Hour() && now.Minute() > startTime.Minute()) {
+		isLate = 1
+	}
 
-		if err := tx.Create(&newlogrecore).Error; err != nil {
-			tx.Rollback()
-			return err
-		}
-		if err := tx.Model(&model.AttendanceLog{}).Where("id =?", attendancelog.ID).Update("shift_session_order", nextOrder).Error; err != nil {
+	newLog := model.AttendanceLog{
+		EmployeeID:            input.EmployeeID,
+		CheckDate:             currentDate,
+		Note:                  "",
+		BranchID:              input.BranchID,
+		StatusAttendanceLogID: 1,
+		ShiftSessionOrder:     shiftOrder,
+	}
+
+	if err := tx.Create(&newLog).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	newRecord := model.AttendanceRecord{
+		AttendanceLogID: newLog.ID,
+		ShiftSessionID:  session.ID,
+		CheckTime:       now,
+		IsLate:          &isLate,
+		IsLeftEarly:     nil,
+		Latitude:        branchLat,
+		Logitude:        branchLng,
+		Note:            input.Note,
+		Iszoone:         inzone,
+		Type:            "IN",
+	}
+
+	if err := tx.Create(&newRecord).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err == nil {
+		if err := tx.Model(&model.AttendanceLog{}).
+			Where("id = ?", attendancelog.ID).
+			Update("shift_session_order", shiftOrder).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
