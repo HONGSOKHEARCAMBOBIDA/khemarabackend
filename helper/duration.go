4d1c4b787ef994121e3f08e7ac2549761e61b592
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-05-09 22:22:27 +0700

    update

--- helper/duration.go
+++ helper/duration.go
@@ -6,6 +6,8 @@ func FormatDuration(totalMinutes float64) (hours float64, display string) {
 	h := int(totalMinutes) / 60
 	m := int(totalMinutes) % 60
 	hours = totalMinutes / 60
-	display = fmt.Sprintf("%dh %dm", h, m)
+	display = fmt.Sprintf("%dម៉ោង %dនាទី", h, m)
+	// fmt.Sprintf() → creates a formatted string
+	// %d → placeholder for an integer number
 	return
 }
