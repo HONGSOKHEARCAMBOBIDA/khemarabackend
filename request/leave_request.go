604bb0008c7b88cbe0b4b89011b0a472fe036495
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-05-12 10:51:57 +0700

    update

--- request/leave_request.go
+++ request/leave_request.go
@@ -4,7 +4,7 @@ type LeaveCreate struct {
 	LeaveTypeID    int     `form:"leave_type_id"`
 	StartDate      string  `form:"start_date"`
 	EndDate        string  `form:"end_date"`
-	BackDate       string  `json:"back_date"`
+	BackDate       string  `form:"back_date" gorm:"column:back_date"`
 	Description    string  `form:"description"`
 	ApproveByID    int     `form:"approve_by"`
 	DurationVlaue  float64 `form:"duration_value"`
